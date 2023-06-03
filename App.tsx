eb80398d4742a12b8c6896576b90854074946e9a
AZDEV2006 <120896393+AZDEV2006@users.noreply.github.com>
2023-06-03 11:54:13 +0700

    add screen

--- App.tsx
+++ App.tsx
@@ -3,17 +3,23 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./pages/HomeScreen";
 import { RootStackParamList } from "./types/navigation";
+import AddScreen from "./pages/AddScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Add">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Home" }}
+        />
+        <Stack.Screen
+          name="Add"
+          component={AddScreen}
+          options={{ title: "Add" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- types/navigation.ts
+++ types/navigation.ts
@@ -2,6 +2,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
   Home: undefined;
+  Add: undefined
 };
 
 export type N<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
